89ff92d4446c8571a6ed92645fac20b1d2c6c1c3
Jean Baptiste Tuyizere <jtuyizere37@gmail.com>
2020-02-15 22:08:15 +0200

    UI: Created and designed login page

---
+++ ui/src/views/Login.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="page-wrapper">
+        <div class="card mt-md-5 login-card">
+            <div class="card-body">
+                <h1 class="display-5 text-center">ESN Login</h1>
+
+                <form class="mt-sm-5">
+                    <div class="form-group">
+                        <label for="username">Username</label>
+                        <input type="text" v-model="username" placeholder="Enter username"
+                               class="form-control"
+                               id="username">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" v-model="password" placeholder="Enter password"
+                               class="form-control" id="password">
+                    </div>
+
+                    <button type="submit" class="btn btn-primary btn-block">Log in</button>
+                    <small class="d-block">
+                        Not already registered? click <a href="">here</a> to register
+                    </small>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login",
+        data() {
+            return {
+                username: '',
+                password: ''
+            }
+        },
+        methods: {
+            login() {
+
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+    .page-wrapper {
+        display: flex;
+        justify-content: center;
+        padding: 8px 16px;
+    }
+
+    .login-card {
+        width: 400px;
+    }
+</style>
