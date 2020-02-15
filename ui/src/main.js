4647ec447612238c9468066e68d25fece439463c
Jean Baptiste Tuyizere <jtuyizere37@gmail.com>
2020-02-15 23:30:35 +0200

    No comment

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,12 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+// import store from './store'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+import './assets/bootstrap.scss'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  store,
+  // store,
   render: h => h(App)
 }).$mount('#app')
