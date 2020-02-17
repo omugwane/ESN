bbb2cc729b1e66af36342b457e42d4c798720bfe
Jean Baptiste Tuyizere <jtuyizere37@gmail.com>
2020-02-17 12:10:44 +0200

    Added functions in the UserController that will be used to handle requests regarding User model CRUD operations

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,3 +1,15 @@
 exports.register_user = function (req, res) {
 
 }
+
+exports.get_all_users = function (req, res) {
+
+}
+
+exports.delete_user = function (req, res) {
+
+}
+
+exports.update_user = function (req, res) {
+
+}
