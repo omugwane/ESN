a055b543907c82dc959589d3d5da376da29a818f
Jean Baptiste Tuyizere <jtuyizere37@gmail.com>
2020-02-17 11:02:34 +0200

    Replaced the API documentation html file with a pdf version

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 import Login from "../views/Login";
 
 Vue.use(VueRouter)
@@ -9,12 +8,12 @@ const routes = [
     {
         path: '/',
         name: 'Home',
-        component: Home
+        component: Login
     },
     {
         path: '/login',
         name: 'login',
-        component: Login
+        component: Login,
     },
     {
         path: '/register',
