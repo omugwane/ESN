481f9e45104f4bfa36c2330f36bc8eecd3666a44
orlymug <orlymug1@gmail.com>
2020-02-18 19:51:58 +0200

    added get all users and create user methods

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,9 +1,31 @@
-exports.registerUser = function (req, res) {
+const User = require('../models/User');
 
+exports.registerUser = function (req, res) {
+    let user = new User();
+    user.content = req.body.content;
+    user.save((err) => {
+        if (err) {
+            res.status(500).json(err);
+        } else {
+            res.status(200).json({ "message" : "success", data: []})
+        }
+    });
 }
 
 exports.getAllUsers = function (req, res) {
 
+    User.find({}, (err, docs) => {
+
+        if (err) {
+            res.status(500).json({ data: Null });
+        } else {
+            let responseObject = {
+                data: docs,
+            }
+            res.status(200).json(responseObject);
+        }
+
+    });
 }
 
 exports.deleteUser = function (req, res) {
