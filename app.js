74e8fd4488e90276bbc4e4afcdc8a62c3d89a939
Aimable Tuyishime <tuyishaim@gmail.com>
2020-02-18 12:34:04 +0200

    Configured MongoDB, body-parser, chat routes

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const configDB = require('./config/DB');
 
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const chatsRouter = require('./routes/chats');
+
+let app = express();
+
+// Set up the database
+mongoose.connect(configDB.database);
+let db = mongoose.connection;
+
+// Check connection
+db.once('open', function() {
+	console.log("Connected to MongoDB");
+});
+
+// Check DB errors
+db.on('error', function(err) {
+	console.log(err);
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,9 +37,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/chats', chatsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -35,7 +57,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send('error');
 });
 
 module.exports = app;
