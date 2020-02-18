f4bd9ffc5b600009e7f03cad062cd875e415dfe0
Laurette Kazenga <lkazenga@go.rwanda.cmu.local>
2020-02-18 20:01:50 +0200

    Added the delete and update methods

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -6,10 +6,24 @@ exports.getAllUsers = function (req, res) {
 
 }
 
-exports.deleteUser = function (req, res) {
+exports.deleteUser = async (req, res)=> {
+    try{
+        const deletedUser = User.remove({_id:req.params.userId});
+        res.status(200).res({ message: "User deleted successfully" });
+    }catch(err){
+        res.status(500).json({message:err});
+    }
+};
 
-}
+exports.updateUser = async (req, res)=> {
+    try{
+        const updatedUser = await User.updateOne(
+            {_id:req.params.userId},
+            {$set:{username:req.body.username}}
+        );
+        res.status(200).res({message:"User updated"});
+    }catch(err){
+        res.status(500).json({message:err});
+    }
+};
 
-exports.updateUser = function (req, res) {
-
-}
