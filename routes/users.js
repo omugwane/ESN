d5179a5dae847421a63fa3668106ba496683ac26
orlymug <orlymug1@gmail.com>
2020-02-18 19:54:05 +0200

    added create user and get all users endpoints

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,6 @@ const userController = require("../controllers/UserController");
 
 /* GET users listing. */
 router.get('/', userController.getAllUsers);
+router.post('/', userController.registerUser);
 
 module.exports = router;
