a979344570802f46b03d8d9a75b180b6bd944051
Jean Baptiste Tuyizere <jtuyizere37@gmail.com>
2020-02-19 02:10:50 +0200

    Added a connection to ATLAS db for testing

--- config/DB.js
+++ config/DB.js
@@ -1,4 +1,5 @@
 module.exports = {
 	database : "mongodb://localhost:27017/esn",
+	// database : 'mongodb+srv://baptiste:bapt_cmu_2020@fse-cluster-rm6b4.mongodb.net/esn?retryWrites=true&w=majority', //For testing Front-end
 	password : '[redacted]'
 }
