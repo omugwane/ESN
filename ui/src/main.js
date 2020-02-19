de26feda23dd832653aca29aacbcf4cdebb69c82
Jean Baptiste Tuyizere <jtuyizere37@gmail.com>
2020-02-19 22:26:59 +0200

    .

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import VueCookies from 'vue-cookies';
@@ -57,6 +56,6 @@ Vue.config.productionTip = false
 
 new Vue({
     router,
-    store,
+    // store,
     render: h => h(App)
 }).$mount('#app')
