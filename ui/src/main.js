628d3bbd56c1765f3ec052cd265eab876b6bc5dc
Jean Baptiste Tuyizere <jtuyizere37@gmail.com>
2020-02-19 02:06:38 +0200

    Added axios which is used to make HTTP requests

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-// import store from './store'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
 
 // Install BootstrapVue
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
+Vue.use(VueAxios, axios)
 import './assets/bootstrap.scss'
 
 //Material design icons
@@ -16,7 +18,7 @@ import '@mdi/font/css/materialdesignicons.css'
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  // store,
-  render: h => h(App)
+    router,
+    // store,
+    render: h => h(App)
 }).$mount('#app')
