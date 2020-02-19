8d9987107135ba3ae04ed109dfc248f12d2fb908
Jean Baptiste Tuyizere <jtuyizere37@gmail.com>
2020-02-19 02:12:47 +0200

    Fixed the bug of specifying the value _id key on the model in registerUser function. Also, changed role on the model in registerUser to use Role.js values

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,23 +1,25 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt-nodejs');
+const mongoose = require('mongoose');
+const Roles = require('../lib/Role');
 
 exports.registerUser = function (req, res) {
     let user = new User();
 
+    user._id = new mongoose.Types.ObjectId();
     user.username = req.body.username;
     user.password = bcrypt.hashSync(req.body.password);
-    user.firstName=req.body.firstName;
-    user.lastName=req.body.lastName;
-    user.email=req.body.email;
-    user.phone=req.body.phone;
-    user.role=req.body.role;
+    user.firstName = req.body.firstName;
+    user.lastName = req.body.lastName;
+    user.email = req.body.email;
+    user.phone = req.body.phone;
+    user.role = Roles.CITIZEN;
 
     user.save((err) => {
         if (err) {
-            
             res.status(500).json(err);
         } else {
-           res.status(200).json({ "message" : "success", data: []})
+            res.status(200).json({"message": "success", data: []})
         }
     });
 }
@@ -27,7 +29,7 @@ exports.getAllUsers = function (req, res) {
     User.find({}, (err, docs) => {
 
         if (err) {
-            res.status(500).json({ data: Null });
+            res.status(500).json({data: Null});
         } else {
             let responseObject = {
                 data: docs,
@@ -38,25 +40,25 @@ exports.getAllUsers = function (req, res) {
     });
 }
 
-exports.deleteUser = async (req, res)=> {
-    User.deleteOne({_id:req.params.userId}, (err, result) => {
+exports.deleteUser = async (req, res) => {
+    User.deleteOne({_id: req.params.userId}, (err, result) => {
         if (err) {
-            res.status(500).json({message:err});
+            res.status(500).json({message: err});
         } else {
-            res.status(200).json({ message: "User deleted successfully" });
+            res.status(200).json({message: "User deleted successfully"});
         }
     });
 };
 
-exports.updateUser = async (req, res)=> {
-    try{
+exports.updateUser = async (req, res) => {
+    try {
         const updatedUser = await User.updateOne(
-            {_id:req.params.userId},
-            {$set:{username:req.body.username}}
+            {_id: req.params.userId},
+            {$set: {username: req.body.username}}
         );
-        res.status(200).json({message:"User updated"});
-    }catch(err){
-        res.status(500).json({message:err});
+        res.status(200).json({message: "User updated"});
+    } catch (err) {
+        res.status(500).json({message: err});
     }
 };
 
