7986c095439ead0da1ec6640c8f6269f91cee261
Laurette Kazenga <lkazenga@go.rwanda.cmu.local>
2020-03-03 23:57:25 +0200

    Removed the getChatByID method

--- routes/chats.js
+++ routes/chats.js
@@ -6,7 +6,6 @@ const chatController = require("../controllers/ChatController");
 /* GET chats listing. */
 router.get('/', chatController.getAllChats);
 router.post('/', chatController.saveChat);
-router.get('/:chatId', chatController.getChatByID);
 router.get('/:username', chatController.getChatsByUsername);
 router.delete('/:chatId', chatController.deleteChat);
 router.patch('/:chatId', chatController.updateChat);
