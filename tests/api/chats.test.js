41ddfaaf6f724cb1b15116f8d5b9de92114d0ac5
Laurette Kazenga <lkazenga@go.rwanda.cmu.local>
2020-03-04 01:19:14 +0200

    Created integration tests for the chat route

--- tests/api/chats.test.js
+++ tests/api/chats.test.js
@@ -15,4 +15,43 @@ describe('Chats routes', () => {
             }
         });
     });
+    
+    test('Should get chats by Username', (done) => {
+        request(app).get('/chats/bapt').then((response) => {
+            try{
+                expect(response.statusCode).toBe(200);
+                done();
+            }catch (e) {
+                done.fail(e);
+            }
+        });
+    });
+
+    test('Should get private chats', (done) => {
+        request(app).get('/chats/bapt/peter').then((response) => {
+            try{
+                expect(response.statusCode).toBe(200);
+                done();
+            }catch (e) {
+                done.fail(e);
+            }
+        });
+    });
+
+    test('Should save chats', (done) => {
+        request(app).post('/chats')
+        .send({
+            sender: 'Alain', 
+            content: 'hey', 
+            status: 'undefined'
+        }).then((response) => {
+            try{
+                expect(response.statusCode).toBe(200);
+                done();
+            }catch (e) {
+                done.fail(e);
+            }
+        });
+    });
+    
 });
