c2ed77f7ebb626424dadbd3b033951c03f422b26
Jean Baptiste Tuyizere <jtuyizere37@gmail.com>
2020-03-21 21:24:38 +0200

    Fixed lint errors

--- repositories/SearchRepository.js
+++ repositories/SearchRepository.js
@@ -2,20 +2,20 @@ const User = require('../models/User');
 
 exports.searchCitizenByUsername = (username, callback) => {
 
-    let callback1 = (err, docs) => {
-        if (err)
-        {
-            console.log("Error from repository");
-            callback(null);
+	let callback1 = (err, docs) => {
+		if (err)
+		{
+			console.log("Error from repository");
+			callback(null);
 
-        }
-        else {
-            console.log("Some users should be displayed");
-            callback(docs);
-        }
-    };
+		}
+		else {
+			console.log("Some users should be displayed");
+			callback(docs);
+		}
+	};
 
-    User.findOne({username: new RegExp('^'+username+'$', "i")}, callback1);
+	User.findOne({username: new RegExp('^'+username+'$', "i")}, callback1);
 
 
 
--- tests/repositories/UserRepository.test.js
+++ tests/repositories/UserRepository.test.js
@@ -21,82 +21,79 @@ afterAll(async () => await dbHandler.closeDatabase());
 
 
 describe('User Repository', () => {
-	it('Dummy',()=>{
-		expect(true).toBe(true);
+	 it('Should save a user successfully', done => {
+		let user = {
+			username: 'me',
+			password: '1234',
+			firstName: 'firstName',
+			lastName: 'lastName',
+			email: 'email',
+			phone: 'phone',
+			role: 'Citizen',
+			status: 'OK'
+		};
+		UserRepository.registerUser(user, (result) => {
+			try {
+				expect(result.username).toBe(user.username);
+				done();
+			} catch (error) {
+				done.fail(error);
+			}
+		});
 	});
-	/* it('Should save a user successfully', done => {
-        let user = {
-            username: 'me',
-            password: '1234',
-            firstName: 'firstName',
-            lastName: 'lastName',
-            email: 'email',
-            phone: 'phone',
-            role: 'Citizen',
-            status: 'OK'
-        }
-        UserRepository.registerUser(user, (result) => {
-            try {
-                expect(result.username).toBe(user.username)
-                done()
-            } catch (error) {
-                done.fail(error)
-            }
-        })
-    })
 
-    it('Should get Users by Username successfully', done => {
-        let user = {
-            username: 'me',
-            password: '1234',
-            firstName: 'firstName',
-            lastName: 'lastName',
-            email: 'email',
-            phone: 'phone',
-            role: 'Citizen',
-            status: 'OK'
-        }
-        UserRepository.registerUser(user, (result) => {
-            try {
-                UserRepository.getUserByUsername(user.username, (users) => {
-                    try {
-                        expect(user.username).not.toBe(null);
-                        done()
-                    } catch (error) {
-                        done.fail(error)
-                    }
-                })
-            } catch (error) {
-                done.fail(error)
-            }
-        });
-    })
+	it('Should get Users by Username successfully', done => {
+		let user = {
+			username: 'me',
+			password: '1234',
+			firstName: 'firstName',
+			lastName: 'lastName',
+			email: 'email',
+			phone: 'phone',
+			role: 'Citizen',
+			status: 'OK'
+		};
+		UserRepository.registerUser(user, (result) => {
+			try {
+				UserRepository.getUserByUsername(user.username, (users) => {
+					try {
+						expect(user.username).not.toBe(null);
+						done();
+					} catch (error) {
+						done.fail(error);
+					}
+				});
+			} catch (error) {
+				done.fail(error);
+			}
+		});
+	});
 
-    it('Should get all users successfully', (done) => {
-        let user = {
-            username: 'me',
-            password: '1234',
-            firstName: 'firstName',
-            lastName: 'lastName',
-            email: 'email',
-            phone: 'phone',
-            role: 'Citizen',
-            status: 'OK'
-        }
-        UserRepository.registerUser(user, (savedUser) => {
-                try {
-                    UserRepository.getAllUsers((users) => {
-                        try {
-                            expect(users.length).toBeGreaterThan(0);
-                            done()
-                        } catch (error) {
-                            done.fail(error)
-                        }
-                    })
-                } catch (error) {
-                    done.fail(error)
-                }
-        });
-    })*/
+	it('Should get all users successfully', (done) => {
+		let user = {
+			username: 'me',
+			password: '1234',
+			firstName: 'firstName',
+			lastName: 'lastName',
+			email: 'email',
+			phone: 'phone',
+			role: 'Citizen',
+			status: 'OK'
+		};
+		UserRepository.registerUser(user, (savedUser) => {
+			try {
+				UserRepository.getAllUsers((users) => {
+					try {
+						expect(users.length).toBeGreaterThan(0);
+						done();
+					} catch (error) {
+						done.fail(error);
+					}
+				});
+			} catch (error) {
+				done.fail(error);
+			}
+		});
+	});
 });
 
