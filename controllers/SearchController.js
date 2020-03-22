85d1e759c0c83fdca0fd49827e765ee456a768fe
Jean Baptiste Tuyizere <jtuyizere37@gmail.com>
2020-03-22 19:55:34 +0200

    Merged UI with Front-end

--- controllers/SearchController.js
+++ controllers/SearchController.js
@@ -11,10 +11,14 @@ exports.search = function (req, res) {
 
     let criteria = factory.createCriteria(context, target, value);
 
-    searchCriteriaExecutor.execute(criteria, (err, results) => {
-        if (!err)
-            res.status(200).json({data: results});
-        else
-            res.status(500).json({data: results, error: err});
-    });
+    console.log("Criteria from factory", criteria);
+    if (criteria) {
+        searchCriteriaExecutor.execute(criteria, (err, results) => {
+            if (!err)
+                res.status(200).json({data: results});
+            else
+                res.status(500).json({data: results, error: err});
+        });
+    } else
+        res.status(500).json({data: [], error: 'Invalid data'});
 };
