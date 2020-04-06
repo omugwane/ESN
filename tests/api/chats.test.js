43813f05ce486748710696da80cecb6f0cc4fc6a
Jean Baptiste Tuyizere <jtuyizere37@gmail.com>
2020-04-06 13:46:10 +0200

    Integration test for uploading a video

--- tests/api/chats.test.js
+++ tests/api/chats.test.js
@@ -15,67 +15,95 @@ afterAll(async () => await dbHandler.closeDatabase());
 
 
 describe('Chats routes', () => {
-	test('Should get all saved public chats', (done) => {
-		request(app).get('/chats').then((response) => {
-			try {
-				expect(response.statusCode).toBe(200);
-				done();
-			} catch (e) {
-				done.fail(e);
-			}
-		});
-	});
+    test('Should get all saved public chats', (done) => {
+        request(app).get('/chats').then((response) => {
+            try {
+                expect(response.statusCode).toBe(200);
+                done();
+            } catch (e) {
+                done.fail(e);
+            }
+        });
+    });
 
-	test('Should get chats by Username', (done) => {
-		request(app).get('/chats/bapt').then((response) => {
-			try {
-				expect(response.statusCode).toBe(200);
-				done();
-			} catch (e) {
-				done.fail(e);
-			}
-		});
-	});
+    test('Should get chats by Username', (done) => {
+        request(app).get('/chats/bapt').then((response) => {
+            try {
+                expect(response.statusCode).toBe(200);
+                done();
+            } catch (e) {
+                done.fail(e);
+            }
+        });
+    });
 
-	test('Should get private chats', (done) => {
-		request(app).get('/chats/bapt/peter').then((response) => {
-			try {
-				expect(response.statusCode).toBe(200);
-				done();
-			} catch (e) {
-				done.fail(e);
-			}
-		});
-	});
+    test('Should get private chats', (done) => {
+        request(app).get('/chats/bapt/peter').then((response) => {
+            try {
+                expect(response.statusCode).toBe(200);
+                done();
+            } catch (e) {
+                done.fail(e);
+            }
+        });
+    });
 
-	test('Should save chats', (done) => {
-		const User = require('../../models/User');
+    test('Should save chats', (done) => {
+        const User = require('../../models/User');
 
-		let user = new User();
-		user.username = 'Alain';
-		user.password = '1234';
-		user.role = 'Citizen';
-		user.status = 'undefined';
+        let user = new User();
+        user.username = 'Alain';
+        user.password = '1234';
+        user.role = 'Citizen';
+        user.status = 'undefined';
 
-		user.save((err) => {
-			if (err)
-				done.fail(err);
+        user.save((err) => {
+            if (err)
+                done.fail(err);
 
-			request(app).post('/chats')
-				.send({
-					sender: user.username,
-					content: 'hey',
-					status: user.status
-				}).then((response) => {
-					try {
-						expect(response.statusCode).toBe(200);
-						done();
-					} catch (e) {
-						done.fail(e);
-					}
+            request(app).post('/chats')
+                .send({
+                    sender: user.username,
+                    content: 'hey',
+                    status: user.status
+                }).then((response) => {
+                try {
+                    expect(response.statusCode).toBe(200);
+                    done();
+                } catch (e) {
+                    done.fail(e);
+                }
 
-				});
-		});
-	});
+            });
+        });
+    });
 
+    test('Should upload a video', (done) => {
+        const User = require('../../models/User');
+
+        let user = new User();
+        user.username = 'Alain';
+        user.password = '1234';
+        user.role = 'Citizen';
+        user.status = 'undefined';
+
+        user.save((err) => {
+            if (err)
+                done.fail(err);
+
+            request(app).post('/chats/upload')
+                .field({content: "This is caption", sender: user.username, receiver: 'null'})
+                .attach('video', 'tests/test_files/rufus.mp4')
+                .end(function (err, response) {
+                    if (err) {
+                        done.fail(err);
+                    } else {
+                        // statusCode
+                        expect(response.status).toBe(200);
+                        done();
+                    }
+                });
+
+        });
+    });
 });
