2919b23d5ae385f2cead471c5ef31376f1cab485
bbukombi <bbukombi@andrew.cmu.edu>
2020-04-13 21:04:52 +0200

    uncommented code in vue.config

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -2,6 +2,7 @@
    Configuring the path to where the vue.js compiled files will placed
    relative to Nodejs back-end root directory
  */
+
 module.exports = {
     outputDir: '../views',
     assetsDir: "../public"
