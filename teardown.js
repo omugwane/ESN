63f299094c3789142446e83b9ed1f31a7b862635
Jean Baptiste Tuyizere <jtuyizere37@gmail.com>
2020-04-18 23:21:48 +0200

    Added code to delete all files uploaded after tests

--- teardown.js
+++ teardown.js
@@ -1,3 +1,28 @@
 module.exports = async function() {
-	// await global.__MONGOD__.stop();
+    //Deleting uploaded images files after tests. Note that all files will be deleted!
+    const fs = require('fs');
+    const path = require('path');
+
+    const imagesDirectory = 'public/uploads/images';
+    fs.readdir(imagesDirectory, (err, files) => {
+        if (err) throw err;
+
+        for (const file of files) {
+            fs.unlink(path.join(imagesDirectory, file), err => {
+                if (err) throw err;
+            });
+        }
+    });
+
+    //Deleting all video files used in testing. Note that all files will be deleted!
+    const videosDirectory = 'public/uploads/videos';
+    fs.readdir(videosDirectory, (err, files) => {
+        if (err) throw err;
+
+        for (const file of files) {
+            fs.unlink(path.join(videosDirectory, file), err => {
+                if (err) throw err;
+            });
+        }
+    });
 };
